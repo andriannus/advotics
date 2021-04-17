0547e619367db2d628361614415698c365ded33a
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-04-17 10:36:35 +0700

    chore: install and setup amar-ui

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        sassOptions: {
+          includePaths: ["./node_modules"],
+        },
+      },
+    },
+  },
+
   chainWebpack(config) {
     config.plugin("html").tap(options => {
       options[0].title = "Vue 3 Template";
