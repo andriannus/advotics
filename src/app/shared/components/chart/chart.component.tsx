ad45350f11326a622a7ab82a8a3da0e2351fcc57
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-04-18 13:36:38 +0700

    feat: create chart component

---
+++ src/app/shared/components/chart/chart.component.tsx
@@ -0,0 +1,53 @@
+import {
+  defineComponent,
+  onBeforeMount,
+  onMounted,
+  PropType,
+  ref,
+  watch,
+} from "vue";
+import { Chart, ChartConfiguration, registerables } from "chart.js";
+
+export default defineComponent({
+  name: "ChartComponent",
+
+  props: {
+    configuration: {
+      type: Object as PropType<ChartConfiguration>,
+      default: null,
+    },
+    id: {
+      type: String,
+      default: null,
+    },
+  },
+
+  setup(props) {
+    const chart = ref<Chart>({} as Chart);
+
+    onBeforeMount(() => {
+      Chart.register(...registerables);
+    });
+
+    onMounted(() => {
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      renderChart();
+    });
+
+    watch(
+      () => props.configuration.data.datasets,
+      () => {
+        chart.value.destroy();
+        // eslint-disable-next-line @typescript-eslint/no-use-before-define
+        renderChart();
+      },
+    );
+
+    function renderChart(): void {
+      const element = document.getElementById(props.id) as HTMLCanvasElement;
+      chart.value = new Chart(element, props.configuration);
+    }
+
+    return (): JSX.Element => <canvas id={props.id}></canvas>;
+  },
+});
---
+++ src/app/shared/components/chart/index.ts
@@ -0,0 +1,1 @@
+export { default as AdvChart } from "./chart.component";
